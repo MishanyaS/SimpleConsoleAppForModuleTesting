d1b03fda5001fc4c1c242fe039d914ff2e8e3e84
Mishanya <shilinm1203@gmail.com>
2021-12-02 05:09:53 +0200

    Добавьте файлы проекта.

---
+++ SimpleConsoleAppForModuleTesting.Tests/MethodsTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SimpleConsoleAppForModuleTesting.Tests
+{
+    public class MethodsTests
+    {
+        private readonly Methods methods;
+
+        public MethodsTests()
+        {
+            methods = new Methods();
+        }
+
+        [Fact]
+        public void Sum_ShouldWork()
+        {
+            //Arrange
+            double expected = 10;
+
+            //Act
+            double actual = methods.Sum(2, 8);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Difference_ShouldWork()
+        {
+            //Arrange
+            double expected = 4;
+
+            //Act
+            double actual = methods.Difference(10, 6);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(20, 5, 4)]
+        public void Devision_ShouldWork(double a, double b, double expected)
+        {
+            //Arrange
+
+            //Act
+            double actual = methods.Devision(a, b);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(3, 5, 15)]
+        public void Multiplication_ShouldWork(double a, double b, double expected)
+        {
+            //Arrange
+
+            //Act
+            double actual = methods.Multiplication(a, b);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Sum_ShouldNotWork()
+        {
+            //Arrange
+            double expected = 15;
+
+            //Act
+            double actual = methods.Sum(2, 8);
+
+            //Assert
+            Assert.NotEqual(expected, actual);
+        }
+
+        [Fact]
+        public void Difference_ShouldNotWork()
+        {
+            //Arrange
+            double expected = 18;
+
+            //Act
+            double actual = methods.Sum(10, 6);
+
+            //Assert
+            Assert.NotEqual(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(25, 5, 4)]
+        public void Devision_ShouldNotWork(double a, double b, double expected)
+        {
+            //Arrange
+
+            //Act
+            double actual = methods.Sum(a, b);
+
+            //Assert
+            Assert.NotEqual(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(3, 7, 15)]
+        public void Multiplication_ShouldNotWork(double a, double b, double expected)
+        {
+            //Arrange
+
+            //Act
+            double actual = methods.Sum(a, b);
+
+            //Assert
+            Assert.NotEqual(expected, actual);
+        }
+    }
+}
---
+++ SimpleConsoleAppForModuleTesting/Methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleAppForModuleTesting
+{
+    public class Methods
+    {
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Difference(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Devision(double a, double b)
+        {
+
+            if (b == 0) throw new DivideByZeroException();
+
+            return a / b;
+        }
+
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
